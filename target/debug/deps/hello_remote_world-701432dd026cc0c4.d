5290a12ff586681cce0b0bbf622edc668712e359
tmtmt <tmtmt2209@gmail.com>
2020-03-07 06:17:50 +0000

    fix

--- target/debug/deps/hello_remote_world-701432dd026cc0c4.d
+++
@@ -1,5 +0,0 @@
-/Users/matsumototakahiro/rust-projects/vso_rust_example/target/debug/deps/hello_remote_world-701432dd026cc0c4.rmeta: test-project/src/main.rs
-
-/Users/matsumototakahiro/rust-projects/vso_rust_example/target/debug/deps/hello_remote_world-701432dd026cc0c4.d: test-project/src/main.rs
-
-test-project/src/main.rs:
--- target/debug/deps/hello_remote_world-c4c69d3bee5fcfe8.d
+++
@@ -1,5 +0,0 @@
-/Users/matsumototakahiro/rust-projects/vso_rust_example/target/debug/deps/hello_remote_world-c4c69d3bee5fcfe8.rmeta: test-project/src/main.rs
-
-/Users/matsumototakahiro/rust-projects/vso_rust_example/target/debug/deps/hello_remote_world-c4c69d3bee5fcfe8.d: test-project/src/main.rs
-
-test-project/src/main.rs:
